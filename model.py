d63957d338ad9aeb91d2fd03b9382577e28ac281
cjcyovo <80971137+cjcyovo@users.noreply.github.com>
2025-03-19 14:22:20 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import CSMamba
+import LDBMamba
 def get_model(args):
-    if args.algorithm == 'CSMamba':
+    if args.algorithm == 'LDBMamba':
         return CSMamba(n_bands=args.n_bands, patch_size=args.patch_size, spa_size=args.spa_size, spe_size=args.spe_size, layer_d_model=args.layer_d_model)
